551c9e8354cae47cb1c4ce24017f564f3d0ddbb4
XariOscar <vitalik.mush@gmail.com>
2024-11-27 19:56:57 +0300

    Add files via upload

---
+++ Lesson2.2.py
@@ -0,0 +1,36 @@
+first = int(input('Введите число'))
+
+second = int(input('Введите число'))
+
+third = int(input('Введите число'))
+
+
+
+if first == second and second == third:
+    print(first, second, third)
+elif first == second:
+    print(first,second)
+elif first == third:
+    print(first, third)
+elif second == third:
+    print(second,third)
+elif first != second and second != third:
+    print(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
