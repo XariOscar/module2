8845f000d93c64cb91c330f073be85fe625d262c
XariOscar <vitalik.mush@gmail.com>
2024-11-27 19:53:59 +0300

    Add files via upload

---
+++ Lesson2.4.py
@@ -0,0 +1,26 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+value = 0
+while value < len(my_list):
+    if my_list[value] < 0 :
+        break
+    if my_list[value] > 0 :
+        print(my_list[value])
+    value += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
